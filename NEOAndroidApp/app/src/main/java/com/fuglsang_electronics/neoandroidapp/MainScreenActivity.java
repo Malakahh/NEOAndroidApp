d69ba7202a11302776a37eb6f882b02ea844e6e6
Malakahh <Malakahh@users.noreply.github.com>
2016-08-09 13:03:04 +0200

    initial project

---
+++ NEOAndroidApp/app/src/main/java/com/fuglsang_electronics/neoandroidapp/MainScreenActivity.java
@@ -0,0 +1,13 @@
+package com.fuglsang_electronics.neoandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
